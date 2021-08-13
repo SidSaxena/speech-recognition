f1f78f829921f17022fd4669d07c649d2000848b
Sid <saxena.siddhu@gmail.com>
2021-08-13 16:19:18 +0530

    server and client scripts for flask server

--- server.py
+++ server.py
@@ -1,4 +1,33 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import os
+import random
+
+from librosa.core import audio 
+from keyword_spotting_service import Keyword_Spotting_Service
 
 app = Flask(__name__)
 
+@app.route('/predict', methods=['POST'])
+def predict():
+
+    # get audio file and save it
+    audio_file = request.files['file']
+    file_name = str(random.randint(0, 100000))
+    audio_file.save(file_name)
+
+    # invoke keyword spotting service
+    kss = Keyword_Spotting_Service()
+
+    # make a prediction
+    predicted_keyword = kss.predict(audio_file)
+
+    # remove the audio file
+    os.remove(file_name)
+
+    # send back the predicted keyword in json format
+    data = {'keyword': predicted_keyword}
+    return jsonify(data)
+
+
+if __name__ == '__main__':
+    app.run(debug=False)
